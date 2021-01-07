3b53afd9474d2899673ba2a2a934a6916f0be4d4
Patryk Orlowski <222530@edu.p.lodz.pl>
2021-01-07 11:57:11 +0100

    Added features: taking cup from handler, picking up ready order

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,36 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+
+import javafx.event.ActionEvent;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    @FXML
+    private Button cupToTake1;
+
+    @FXML
+    private Button cupToTake2;
+
+    @FXML
+    private Button takenCup;
+
+    @FXML
+    public void takeCupFromHandler(ActionEvent event) {
+        takenCup.setStyle("-fx-opacity: 1;");
+    }
+
+    @FXML
+    public void pickUpOrder(ActionEvent event) {
+            takenCup.setStyle("-fx-opacity: 0;");
+    }
 }
