6fab5b38b43696022afc194a38ffdadd06d9afed
Karolina Woźnicka <222618@edu.p.lodz.pl>
2021-01-07 14:47:44 +0100

    Added sugar and cafe graphics

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,10 +7,18 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
 
 import javafx.event.ActionEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -25,10 +33,23 @@ public class Controller implements Initializable {
     @FXML
     private Button takenCup;
 
+    @FXML
+    public Pane cafe;
 
     @FXML
     public void takeCupFromHandler(ActionEvent event) {
         takenCup.setStyle("-fx-opacity: 1;");
+    }
+
+    @FXML
+    public void startCafeHandler(ActionEvent event) {
+        cafe.setStyle("-fx-opacity: 1;");
+        //for(int i=0, n=1; i<1000000000; i++, n++){
+            //if(n>3) n=1;
+            //cafe.setStyle("-fx-background-image: url('images/kawa"+ n +".png');");
+        //}
+        //cafe.setStyle("-fx-opacity: 0;");
+
     }
 
     @FXML
