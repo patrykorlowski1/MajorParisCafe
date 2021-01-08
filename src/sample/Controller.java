f2a0662930e22c3b5eb88f998c1c34db2546b264
Patryk Orlowski <222530@edu.p.lodz.pl>
2021-01-08 12:01:19 +0100

    Deleted not needed comments

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,21 +1,14 @@
 package sample;
 
 import javafx.application.Platform;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
-
 import javafx.event.ActionEvent;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -85,17 +78,6 @@ public class Controller implements Initializable {
             {
                 try
                 {
-                    for(int i=0, n=1; i<100; i++)
-                    {
-                        n++;
-                        if(n>3) n=1;
-                        //tu ma sie przelaczac obrazek
-                        //Thread.sleep(1000);
-                        //if(n==1) cafe.setStyle("-fx-background-image: url('images/kawa1.png');");
-                        //else if(n==2) cafe.setStyle("-fx-background-image: url('images/kawa2.png');");
-                        //else if(n==3) cafe.setStyle("-fx-background-image: url('images/kawa3.png');");
-                        //Thread.sleep(5000);
-                    }
                     Thread.sleep(5000);
                     cafe.setStyle("-fx-opacity: 0;");
                     toggleButtons();
