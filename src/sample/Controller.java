a1c191209791a026105fe06dac45b41f677a2015
Karolina Woźnicka <222618@edu.p.lodz.pl>
2021-01-08 14:07:12 +0100

    added cafe animation and adapted the sound to that

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -29,6 +29,7 @@ public class Controller implements Initializable {
     public Button sugar5;
     public Button sugar6;
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -45,6 +46,8 @@ public class Controller implements Initializable {
 
     @FXML
     public Pane cafe;
+    public Pane cafe2;
+    public Pane cafe3;
 
     @FXML
     public void takeCupFromHandler(ActionEvent event) {
@@ -64,28 +67,45 @@ public class Controller implements Initializable {
     @FXML
     public void startCafeHandler(ActionEvent event) {
         if(late.isSelected() || espresso.isSelected()){
-            cafe.setStyle("-fx-background-image: url('images/kawa1.png');");
-            cafe.setStyle("-fx-opacity: 1;");
-
-            if(late.isSelected()) start.setText("Caffe latte");
-            if(espresso.isSelected()) start.setText("Espresso");
 
             Media cafeAudio = new Media(this.getClass().getResource("images/cafe_audio.mp3").toString());
             MediaPlayer cafeAudioPlayer = new MediaPlayer(cafeAudio);
-            cafeAudioPlayer.play();
 
             new Thread(() ->
             {
                 try
                 {
-                    Thread.sleep(5000);
-                    cafe.setStyle("-fx-opacity: 0;");
-                    toggleButtons();
-                    Platform.runLater(()->{
-                        start.setText("START");
-                    });
-                    Thread.currentThread().interrupt();
+                    while(true){
+                        cafeAudioPlayer.play();
+                    }
 
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }).start();
+
+            if(late.isSelected()) start.setText("Caffe latte");
+            if(espresso.isSelected()) start.setText("Espresso");
+
+            new Thread(() ->
+            {
+                try
+                {
+                    Thread.sleep(6500);
+                    for(int i=0, n=1; i<280; i++)
+                    {
+                        n++;
+                        if(n>3) n=1;
+                        Thread.sleep(100);
+                        switchCafeAnimation(n);
+                    }
+
+                    setOffCafeAnimation();
+                    toggleButtons();
+                    Platform.runLater(()->{ start.setText("START"); });
+                    Thread.currentThread().interrupt();
                 }
                 catch (Exception e)
                 {
@@ -110,6 +130,30 @@ public class Controller implements Initializable {
     public void toggleButtons() {
         espresso.setSelected(false);
         late.setSelected(false);
-
     }
+
+    public void setOffCafeAnimation(){
+        cafe2.setStyle("-fx-opacity: 0;");
+        cafe3.setStyle("-fx-opacity: 0;");
+        cafe.setStyle("-fx-opacity: 0;");
+    }
+
+    public void switchCafeAnimation(int n){
+        if(n==1){
+            cafe.setStyle("-fx-opacity: 1;");
+            cafe2.setStyle("-fx-opacity: 0;");
+            cafe3.setStyle("-fx-opacity: 0;");
+        }
+        else if(n==2) {
+            cafe.setStyle("-fx-opacity: 0;");
+            cafe2.setStyle("-fx-opacity: 1;");
+            cafe3.setStyle("-fx-opacity: 0;");
+        }
+        else if(n==3) {
+            cafe.setStyle("-fx-opacity: 0;");
+            cafe2.setStyle("-fx-opacity: 0;");
+            cafe3.setStyle("-fx-opacity: 1;");
+        }
+    }
+
 }
